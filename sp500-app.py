b5229f6c1ab5e771ce272c654bd09c9ea7265e48
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-01-07 13:09:40 -0300

    Add plots

--- sp500-app.py
+++ sp500-app.py
@@ -42,7 +42,7 @@ st.dataframe(df_selected_sector)
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64decode(csv.encode()).decode() # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode() # strings <-> bytes conversions
     href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
     
     return href
@@ -61,5 +61,21 @@ data = yf.download(
         proxy = None
     )
 
+# Plot Closing Price of Query Symbol
+def price_plot(symbol):
+  df = pd.DataFrame(data[symbol].Close)
+  df['Date'] = df.index
+  plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+  plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+  plt.xticks(rotation=90)
+  plt.title(symbol, fontweight='bold')
+  plt.xlabel('Date', fontweight='bold')
+  plt.ylabel('Closing Price', fontweight='bold')
+  return st.pyplot()
 
+num_company = st.sidebar.slider('Number of Companies', 1, 10)
 
+if st.button('Show Plots'):
+    st.header('Stock Closing Price')
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
